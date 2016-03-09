d1b84c813e4985c0be829e1d962858609670760a
Bruno Domingos <dotico@gmail.com>
2016-03-09 17:40:39 +0000

    Add login route and authenticate class method

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
+var bcrypt = require('bcrypt');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -66,18 +67,6 @@ app.post('/todos', function(req, res) {
     });
 });
 
-// POST /users
-app.post('/users', function(req, res) {
-    var body = _.pick(req.body, 'email', 'password');
-    body.email = body.email.trim();
-
-    db.user.create(body).then(function(user) {
-        res.json(user.toJSON());
-    }, function(e) {
-        res.status(400).json(e);
-    });
-});
-
 // DELETE /todos/:id
 app.delete('/todos/:id', function(req, res) {
     var todoId = parseInt(req.params.id, 10);
@@ -115,7 +104,7 @@ app.put('/todos/:id', function(req, res) {
     db.todo.findById(todoId).then(function(todo) {
         if (todo) {
             return todo.update(attributes).then(function(todo) {
-                res.json(todo.toPublicJSON());
+                res.json(todo.toJSON());
             }, function(e) {
                 res.status(400).json(e);
             });
@@ -129,7 +118,30 @@ app.put('/todos/:id', function(req, res) {
 });
 
 
-db.sequelize.sync().then(function() {
+// POST /users
+app.post('/users', function(req, res) {
+    var body = _.pick(req.body, 'email', 'password');
+    body.email = body.email.trim();
+
+    db.user.create(body).then(function(user) {
+        res.json(user.toPublicJSON());
+    }, function(e) {
+        res.status(400).json(e);
+    });
+});
+
+// POST /users/login
+app.post('/users/login', function(req, res) {
+    var body = _.pick(req.body, 'email', 'password');
+
+    db.user.authenticate(body).then(function (user) {
+        res.json(user.toPublicJSON());
+    }, function () {
+        return res.status(401).send();
+    });
+});
+
+db.sequelize.sync({force:true}).then(function() {
     app.listen(PORT, function() {
         console.log('Express listening on port' + PORT + '!');
     });
